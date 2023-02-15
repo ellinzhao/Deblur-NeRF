febedd7cef34a29bed60ff8f27a3548e758bc13a
Ellin Zhao <ellinqzhao@gmail.com>
2023-02-14 17:03:13 -0800

    calculate poses from blender dataset

--- scripts/synthe2poses.py
+++ scripts/synthe2poses.py
@@ -1,102 +1,68 @@
-import os
-import numpy as np
 import json
+import os
+
 import cv2
+import numpy as np
 
-data_path = "./blurtanabatabad"
 
-# smart figure out path
-input_path = os.path.join(data_path, 'raw')
-json_path = os.path.join(input_path, "transforms.json")
-out_path = os.path.join(data_path, "images_1")
-pose_out_path = os.path.join(data_path, "poses_bounds.npy")
-if not os.path.exists(out_path):
-    os.makedirs(out_path, exist_ok=True)
-opengl2opencv = np.array([1, 0, 0, 0,
-                          0, -1, 0, 0,
-                          0, 0, -1, 0,
-                          0, 0, 0, 1], dtype=np.float32).reshape(4,4)
+OPENGL2OPENCV = np.array([
+    1, 0, 0, 0,
+    0, -1, 0, 0,
+    0, 0, -1, 0,
+    0, 0, 0, 1,
+], dtype=np.float32).reshape(4, 4)
 
-# ================================================
-# Blurxxx
-# ================================================
-if "blur" in data_path:
+
+def config_parser():
+    import configargparse
+    parser = configargparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str)
+    parser.add_argument('--bd_min', type=float, default=0.1)
+    parser.add_argument('--bd_max', type=float, default=10.)
+    return parser
+
+
+def main():
+    parser = config_parser()
+    args = parser.parse_args()
+    data_path = args.data_path
+    input_path = os.path.join(data_path, 'images')
+    json_path  = os.path.join(data_path, 'transforms.json')
+    out_path   = os.path.join(data_path, 'images_1')
+    pose_out_path = os.path.join(data_path, 'poses_bounds.npy')
+    if not os.path.exists(out_path):
+        os.makedirs(out_path, exist_ok=True)
+
     with open(json_path, 'r') as metaf:
         meta = json.load(metaf)
-        frames_data = meta["frames"]
-        hold = meta["llffhold"]
-        fov = meta["fov"]
-        h, w = meta['h'], meta['w']
-        cloest = meta['cloest']
+        frames_data = meta['frames']
+        h, w, f = meta['h'], meta['w'], meta['fl_x']
 
-    f = w / 2 / np.tan(fov / 2)
     hwf = np.array([h, w, f]).reshape([3, 1])
     pose_arr = []
+
     for i, frame_data in enumerate(frames_data):
-        pose = frame_data["transform_matrix"]
+        pose = frame_data['transform_matrix']
         pose = np.array(pose, dtype=np.float32)
-        pose = opengl2opencv @ np.linalg.inv(pose)
+        pose = OPENGL2OPENCV @ np.linalg.inv(pose)
         pose = np.linalg.inv(pose)[:3]
         pose = np.concatenate([pose, hwf], 1)
-        pose = np.concatenate([pose[:, 1:2], pose[:, 0:1], -pose[:, 2:3], pose[:, 3:4], pose[:, 4:5]],
-                               1)
-        pose = np.concatenate([pose.reshape(-1), np.array((cloest, cloest + 100), dtype=np.float32)])
+        pose = np.concatenate(
+            [pose[:, 1:2], pose[:, 0:1], -pose[:, 2:3], pose[:, 3:4], pose[:, 4:5]], 1,
+        )
+        pose = np.concatenate(
+            [pose.reshape(-1), np.array((args.bd_min, args.bd_max), dtype=np.float32)]
+        )
         pose_arr.append(pose)
-        assert (i % hold == 0) == (frame_data["blurcount"] == 0)  # valid check
 
-        if i % hold == 0:
-            img = cv2.imread(f"{input_path}/{i:03d}.png")
-            cv2.imwrite(f"{out_path}/{i:03d}.png", img)
-            print(f"frame {i} saved!")
-        else:
-            imgs = []
-            for bluri in range(frame_data["blurcount"]):
-                img = cv2.imread(f"{input_path}/{i:03d}_{bluri:03d}.png")
-                imgs.append(img / 255)
-            
-            imgs = np.array(imgs)
-            imgs = imgs ** 2.2
-            weight = np.ones_like(imgs) * (1 / len(imgs))
-            img = np.sum(imgs * weight, axis=0)
-            img = img ** (1 / 2.2)
-            img = (np.clip(img, 0, 1) * 255).astype(np.uint8)
-            cv2.imwrite(f"{out_path}/{i:03d}.png", img)
-            print(f"frame {i} fused and saved!")
+        img = cv2.imread(f'{input_path}/{i+1:04d}.png')
+        cv2.imwrite(f'{out_path}/{i+1:04d}.png', img)
+        print(f'Frame {i} saved!')
     
     pose_arr = np.stack(pose_arr)
     np.save(pose_out_path, pose_arr)
-    print("poses_bounds.npy saved")
+    return True
 
-# ================================================
-# Defocusxxx
-# ================================================
-else:
-    with open(json_path, 'r') as metaf:
-        meta = json.load(metaf)
-        frames_data = meta["frames"]
-        hold = meta["llffhold"]
-        fov = meta["fov"]
-        h, w = meta['h'], meta['w']
-        cloest = meta['cloest']
 
-    f = w / 2 / np.tan(fov / 2)
-    hwf = np.array([h, w, f]).reshape([3, 1])
-    pose_arr = []
-    for i, frame_data in enumerate(frames_data):
-        pose = frame_data["transform_matrix"]
-        pose = np.array(pose, dtype=np.float32)
-        pose = opengl2opencv @ np.linalg.inv(pose)
-        pose = np.linalg.inv(pose)[:3]
-        pose = np.concatenate([pose, hwf], 1)
-        pose = np.concatenate([pose[:, 1:2], pose[:, 0:1], -pose[:, 2:3], pose[:, 3:4], pose[:, 4:5]],
-                               1)
-        pose = np.concatenate([pose.reshape(-1), np.array((cloest, cloest + 100), dtype=np.float32)])
-        pose_arr.append(pose)
-
-        img = cv2.imread(f"{input_path}/{i:03d}.png")
-        cv2.imwrite(f"{out_path}/{i:03d}.png", img)
-        print(f"frame {i} saved!")
-    
-    pose_arr = np.stack(pose_arr)
-    np.save(pose_out_path, pose_arr)
-    print("poses_bounds.npy saved")
+if __name__ == '__main__':
+    main()
